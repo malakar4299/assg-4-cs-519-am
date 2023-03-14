6cee3d7a0f56483d40fb77aada7b7e9f2a55eb5c
Abhishek <Abhishek@Shaq-Malarkey.local>
2023-03-13 23:46:57 -0400

    Scripts and init texts

---
+++ shell.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+brew update
+brew install azure-cli
+az login
+az account set --subscription 50018cb8-40f8-4590-9abe-d5cfe92f0f67
+value=$(az functionapp keys set -g assg-3-cs-519-am -n assg-3-am --key-type functionKeys --key-name test-key | jq -r '.value')
+
+echo "{ \"code\":\"$value\" }" > config.json
+touch config.json
